223d2e84a9301ab02cddd21b27bfa28d04066d6d
molliemorrow <mfmorrow97@gmail.com>
2021-08-10 08:29:19 -0700

    Created Spring-Boot project

---
+++ ACEBot/src/main/java/com/revature/ACEBot/AceBotApplication.java
@@ -0,0 +1,13 @@
+package com.revature.ACEBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AceBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AceBotApplication.class, args);
+	}
+
+}
---
+++ ACEBot/src/test/java/com/revature/ACEBot/AceBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.ACEBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AceBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
