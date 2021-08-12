29096aa904f62c76a449ef991cbe7c3e6613b9fe
ajang06 <aronjang97@gmail.com>
2021-08-12 14:47:50 -0500

    Added buy and sell

--- pybot/main.py
+++ pybot/main.py
@@ -21,7 +21,28 @@ async def on_message(message):
         await message.channel.send('Howdy! :cowboy:')
 
     if message.content.startswith('$money'):
-        r = requests.get('https://api.coinbase.com/v2/prices/BTC-USD/buy') \
+        btc = requests.get('https://api.coinbase.com/v2/prices/BTC-USD/buy') \
+            .json().get('data')
+        await message.channel.send(
+            btc.get('base') + " -> " + btc.get('currency')
+            + " $" + btc.get('amount'))
+        eth = requests.get('https://api.coinbase.com/v2/prices/ETH-USD/buy') \
+            .json().get('data')
+        await message.channel.send(
+            eth.get('base') + " -> " + eth.get('currency')
+            + " $" + eth.get('amount'))
+
+    if message.content.startswith('$buy-'):
+        symbol = message.content.split('-')
+        r = requests.get('https://api.coinbase.com/v2/prices/'+symbol[1]+'-USD/buy') \
+            .json().get('data')
+        await message.channel.send(
+            r.get('base') + " -> " + r.get('currency')
+            + " $" + r.get('amount'))
+
+    if message.content.startswith('$sell-'):
+        symbol = message.content.split('-')
+        r = requests.get('https://api.coinbase.com/v2/prices/'+symbol[1]+'-USD/sell') \
             .json().get('data')
         await message.channel.send(
             r.get('base') + " -> " + r.get('currency')
