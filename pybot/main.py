bcd061c3e42f2c8a41513224b5eb334e390d74d9
ddinh <21209173+ddinh@users.noreply.github.com>
2021-08-12 15:46:17 -0500

    Add logging

--- pybot/main.py
+++ pybot/main.py
@@ -2,9 +2,16 @@ import os
 import discord
 import requests
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
 client = discord.Client()
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 
 @client.event
