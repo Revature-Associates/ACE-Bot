40aa76a2568b6932250df28d4e5f56dd656e95b0
FBThornton <97fthornton@gmail.com>
2021-08-12 17:37:39 -0400

    Changed from client to bot

--- pybot/main.py
+++ pybot/main.py
@@ -1,51 +1,61 @@
 import os
 import discord
+from discord.ext import commands
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 client = discord.Client()
+bot = commands.Bot(command_prefix='$')
 
 
-@client.event
+@bot.command()
+async def money(ctx, *args):
+    apiurl = 'https://api.coinbase.com/v2/prices/{}-{}/{}'
+    apiargs = {'base':'BTC', 'to':'USD', 'buyorsell':'buy'}
+
+    if(len(args) == 1):
+        apiargs['base'] = args[0]
+    
+    elif(len(args) == 2):
+        apiargs['base'] = args[1]
+        apiargs['buyorsell'] = args[0]
+
+    r = requests.get(apiurl.format(apiargs.get('base'), apiargs.get('to'), apiargs.get('buyorsell')))
+
+    if(r.status_code == 200):
+        r = r.json().get('data')
+        await ctx.send(r.get('base') + " -> " + r.get('currency')
+                + " $" + r.get('amount'))
+
+    else:
+        await ctx.send("You messed up dummy!")
+
+@bot.command()
+async def buy(ctx, arg):
+    r = requests.get('https://api.coinbase.com/v2/prices/{}-USD/buy'.format(arg))
+    if(r.status_code == 200):
+        r = r.json().get('data')
+        await ctx.send(r.get('base') + " -> " + r.get('currency')
+                + " $" + r.get('amount'))
+
+    else:
+        await ctx.send("You messed up dummy!")
+
+@bot.command()
+async def sell(ctx, arg):
+    r = requests.get('https://api.coinbase.com/v2/prices/{}-USD/sell'.format(arg))
+    if(r.status_code == 200):
+        r = r.json().get('data')
+        await ctx.send(r.get('base') + " -> " + r.get('currency')
+                + " $" + r.get('amount'))
+
+    else:
+        await ctx.send("You messed up dummy!")
+
+
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('----BOT READY----')
 
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Howdy! :cowboy:')
-
-    if message.content.startswith('$money'):
-        btc = requests.get('https://api.coinbase.com/v2/prices/BTC-USD/buy') \
-            .json().get('data')
-        await message.channel.send(
-            btc.get('base') + " -> " + btc.get('currency')
-            + " $" + btc.get('amount'))
-        eth = requests.get('https://api.coinbase.com/v2/prices/ETH-USD/buy') \
-            .json().get('data')
-        await message.channel.send(
-            eth.get('base') + " -> " + eth.get('currency')
-            + " $" + eth.get('amount'))
-
-    if message.content.startswith('$buy '):
-        symbol = message.content.split(' ')
-        r = requests.get('https://api.coinbase.com/v2/prices/'+symbol[1]+'-USD/buy') \
-            .json().get('data')
-        await message.channel.send(
-            r.get('base') + " -> " + r.get('currency')
-            + " $" + r.get('amount'))
-
-    if message.content.startswith('$sell '):
-        symbol = message.content.split(' ')
-        r = requests.get('https://api.coinbase.com/v2/prices/'+symbol[1]+'-USD/sell') \
-            .json().get('data')
-        await message.channel.send(
-            r.get('base') + " -> " + r.get('currency')
-            + " $" + r.get('amount'))
-
-client.run(os.getenv('TOKEN'))
+bot.run(os.getenv('TOKEN'))
